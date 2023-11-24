160e2e04c5f18ac7c24388ae3312e779597b1bfe
Fuad <foad.alshabibi@gmail.com>
2023-11-25 01:32:20 +0300

    Begin of Assignment

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = 'Fuad Shop - Main page';
+include 'compounts/head.php';
+?>
 
+<body>
+    <?php
+    include 'compounts/nav.php';
+    ?>
+
+</body>
 
 </html>
